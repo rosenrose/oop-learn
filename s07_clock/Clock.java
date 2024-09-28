1fac6a9d3d53be6aef8a576dd0bc0c78d889514e
rosenrose <crazytempler@gmail.com>
2024-09-28 19:31:16 +0900

    methods for analog

--- s07_clock/Clock.java
+++ s07_clock/Clock.java
@@ -3,6 +3,9 @@ package s07_clock;
 public class Clock {
     private int timeInSecond = 0;
     static final int HALF_DAY_IN_SECONDS = 60 * 60 * 12;
+    static final int ANGLE_PER_HOUR_IN_DEGREE = 360 / 12;
+    static final int ANGLE_PER_MIN_IN_DEGREE = 360 / 60;
+    static final int ANGLE_PER_SEC_IN_DEGREE = 360 / 60;
 
     public byte getHour() {
         int hour = this.timeInSecond / 60 / 60;
@@ -28,16 +31,24 @@ public class Clock {
         this.timeInSecond = seconds;
     }
 
-    public short getHourAngle() {
-        return (short) ((getHour() * 30) + (getMinute() / 2));
+    public short getHourAngleInDegree() {
+        return (short) ((getHour() % 12 * ANGLE_PER_HOUR_IN_DEGREE) + (getMinute() * ANGLE_PER_HOUR_IN_DEGREE / 60));
     }
 
-    public short getMinuteAngle() {
-        return (short) ((getMinute() * 6) + (getSecond() / 10));
+    public short getMinuteAngleInDegree() {
+        return (short) ((getMinute() * ANGLE_PER_MIN_IN_DEGREE) + (getSecond() * ANGLE_PER_MIN_IN_DEGREE / 60));
     }
 
-    public short getSecondAngle() {
-        return (short) (getSecond() * 6);
+    public short getSecondAngleInDegree() {
+        return (short) (getSecond() * ANGLE_PER_SEC_IN_DEGREE);
+    }
+
+    public void tick() {
+        addSeconds((short) 1);
+    }
+
+    public void mount() {
+        System.out.println("Mounted on the wall");
     }
 
     public String toString() {
--- s07_clock/Program.java
+++ s07_clock/Program.java
@@ -12,5 +12,13 @@ public class Program {
 
         clock.addSeconds((short) 100);
         System.out.println(clock);
+        System.out.println();
+
+        clock = new Clock();
+        clock.addSeconds((short) 12615);
+        System.out.println(clock);
+        System.out.printf("hour angle: %d%n", clock.getHourAngleInDegree());
+        System.out.printf("minute angle: %d%n", clock.getMinuteAngleInDegree());
+        System.out.printf("second angle: %d%n", clock.getSecondAngleInDegree());
     }
 }
